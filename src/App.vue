4ded75540616ad4997a25f8e54c34b64ee3d35f0
Vojtěch Ducháček <vojtech.duchacek@gmail.com>
2021-06-23 01:16:25 +0200

    removed boilerplate, added icons, managed to get map displayed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -15,11 +14,35 @@ export default {
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  html
+    height 100%
+    box-sizing border-box
+    font-family sans-serif;
+  *
+    margin 0
+    padding 0
+  * *:before *:after
+    box-sizing border-box
+  html, body
+    min-height 100%
+    height 100%
+    display block
+  #app
+    min-height 100%
+    height 100%
+    width 100%
+    position absolute
+    top: 0
+    left: 0
+    z-index 2
+  #map-container
+    min-height 100%
+    height 100%
+    width 100%
+    display flex
+    position absolute
+    top: 0
+    left: 0
+    z-index 1
+    background #1B1B1E
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import { BlueMapApp } from './js/BlueMapRenderer'
 
-createApp(App).mount('#app')
+// bluemap app
+const bluemap = new BlueMapApp(document.getElementById('map-container'))
+window.bluemap = bluemap
+
+// vue
+const vue = createApp(App).mount('#app')
+
+// load bluemap next tick (to let the assets load first)
+vue.$nextTick(() => {
+  bluemap.load().catch(error => console.error(error))
+})
